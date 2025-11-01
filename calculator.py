81123df4895ba0efec077ffea9f851d7741fca77
Asaw89 <alan691saw@aol.com>
2025-11-01 17:53:35 -0400

    Added exponents and inverse

--- calculator.py
+++ calculator.py
@@ -3,16 +3,26 @@ class Calculator:
     def __init__(self):
         pass
 
-    def additon(self, x, y):
+    def add(self, x, y):
         return x + y
 
-    def subtraction(self, x, y):
+    def sub(self, x, y):
         return x - y
     
-    def division(self, x, y):
+    def div(self, x, y):
         return x / y
     
-    def multiplication(self, x, y):
+    def multi(self, x, y):
         return x * y
+    
+    def square(self, x):
+        return x * x
+    
+    def expo(self, x, y):
+        return x ** y
+    
+    def inverse(self, x):
+        return 1/x
+
 
 # add lots more methods to this calculator class.
--- main-app.py
+++ main-app.py
@@ -6,28 +6,45 @@ def getTwoNumbers():
     b = float(input("second number? "))
     return a, b
 
+def getOneNumber():
+    a = float(input("first number? "))
+    return a
+
+
 
 def displayResult(x: float):
     print(x, "\n")
 
 
 def performCalcLoop(calc):
+    state = 0 
     while True:
-        choice = input("What Do You Want? ")
+        print 
+        choice = input("How can I Help You? ")
         if choice == 'q':
             break  # user types q to quit calulator.
-        elif choice == 'addition':
+        elif choice == 'add':
             a, b = getTwoNumbers()
-            displayResult(calc.addition(a, b))
-        elif choice == 'subtraction':
+            displayResult(calc.add(a, b))
+        elif choice == 'sub':
             a, b = getTwoNumbers()
-            displayResult(calc.subtraction(a, b))
-        elif choice == 'division':
+            displayResult(calc.sub(a, b))
+        elif choice == 'div':
             a, b = getTwoNumbers()
-            displayResult(calc.division(a, b))
-        elif choice == 'multiplication':
+            displayResult(calc.div(a, b))
+        elif choice == 'multi':
             a, b = getTwoNumbers()
-            displayResult(calc.multiplication(a, b))
+            displayResult(calc.multi(a, b))
+        elif choice == 'square':
+            a= getOneNumber()
+            displayResult(calc.square(a))
+        elif choice == 'expo':
+            a, b= getTwoNumbers()
+            displayResult(calc.expo(a, b))
+        elif choice == 'inverse':
+            a=getOneNumber()
+            displayResult(calc.inverse(a))
+        
             
         else:
             print("That is not a valid input.")
